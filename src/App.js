3e35cd4303e0a53e5c020f504566f2e764ea45c7
sagar1024 <141512763+sagar1024@users.noreply.github.com>
2023-10-06 18:08:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React, { useState } from 'react';
 import './App.css';
 import BookList from './BookList';
 import SearchBar from './SearchBar';
-import books from './books';
+import books from './books'; // Import the local JavaScript file with books
 
 function App() {
   const [bookList] = useState(books);
-
   const addReview = (bookId, review) => {
     console.log(`Adding review for book ID ${bookId}: ${review}`);
   };
 
+  const [filteredBooks, setFilteredBooks] = useState(books);
+
+  const handleSearch = (searchResults) => {
+    setFilteredBooks(searchResults);
+  };
+
   return (
     <div className="App">
-      <h1>Online Bookstore</h1>
-      <SearchBar />
-      <BookList books={bookList} addReview={addReview} />
+      <h1>Online BookStore</h1>
+      <SearchBar books={books} onSearch={handleSearch} />
+
+      {/* <BookList books={bookList} addReview={addReview} /> */}
+
+      <ul>
+        {filteredBooks.map((book) => (
+          <li key={book.id}>
+            {book.title} by {book.author} (Rating: {book.rating})
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
